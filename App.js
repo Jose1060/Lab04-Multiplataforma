a85c9bcd6fc762ae609fe951b38d8860999fe41b
Jose1060 <josean-1060@hotmail.com>
2022-04-14 11:09:29 -0500

    PrimerCommit

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Card } from "react-native-paper";
+import OurFlatList from "./app/components/ourFlatList/OurFlatList";
+import ConexionFetch from "./app/components/conexionFetch/ConexionFetch";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Card>
+				<ConexionFetch />
+				<OurFlatList />
+			</Card>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
---
+++ app/components/conexionFetch/ConexionFetch.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { View, FlatList, StyleSheet, Text, Image } from "react-native";
+
+const Item = ({ title, image }) => {
+	return (
+		<View style={styles.item}>
+			<Image source={{ uri: image }} style={{ height: 40, width: 40 }} />
+		</View>
+	);
+};
+
+const options = {
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Host": "movies-app1.p.rapidapi.com",
+		"X-RapidAPI-Key": "8d858cf0a2msh33bca5adb9ce37dp156faejsnae7375738007",
+	},
+};
+
+export default class conexionFetch extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			textValues: 0,
+			coutn: 0,
+			items: [],
+			error: null,
+		};
+	}
+
+	async componentDidMount() {
+		await fetch("https://movies-app1.p.rapidapi.com/api/genres", options)
+			.then((response) => response.json())
+			.then((response) => console.log("response -->", response))
+			.then(
+				(result) => {
+					console.warn("result -->", result);
+					this.setState({
+						items: result.data.movies,
+					});
+				},
+				(error) => {
+					this.setState({
+						error: error,
+					});
+				}
+			)
+			.catch((err) => console.error(err));
+	}
+	render() {
+		return (
+			<View style={styles.container}>
+				<FlatList
+					data={this.state.items.length > 0 ? this.state.items : []}
+					renderItem={({ item }) => (
+						<Item title={item.title} image={item.small_cover_image} />
+					)}
+					keyExtractor={(item) => item.id}
+				/>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		marginTop: 20,
+	},
+	item: {
+		backgroundColor: "rgba(185, 71, 253, 0.29)",
+		padding: 20,
+		marginVertical: 8,
+		marginHorizontal: 16,
+	},
+	title: {
+		fontSize: 32,
+	},
+});
